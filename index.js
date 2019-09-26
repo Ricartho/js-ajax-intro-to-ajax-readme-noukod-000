af3187f04bea4ed0f6d601256796ebf2346b3c9e
Ricartho Franck <ricartho.franck@esih.edu>
2019-09-26 13:39:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+function showRepositories(){
+  console.log(this.responseText);
+}
 function getRepositories(){
   const req = new XMLHttpRequest();
   req.open('GET','https://api.github.com/users/octocat/repos');
