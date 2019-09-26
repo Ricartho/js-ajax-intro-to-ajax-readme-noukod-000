d92119c890cbfcd9ed8a2ec6fcc6adb3311d4e84
Ricartho Franck <ricartho.franck@esih.edu>
2019-09-26 14:21:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function getCommits(el){
 
 function showCommits(){
   const commits = JSON.parse(this.responseText);
-  const commitList = `<ul>${commits.map(commit =>)}`
+  const commitList = `<ul>${commits.map(commit =>'<li>'+commit.author.login'</li>')}</ul>`
 }
 function showRepositories(){
   const repos = JSON.parse(this.responseText);
