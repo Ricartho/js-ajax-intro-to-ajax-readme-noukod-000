3a0bb47f5fef9d69ac9fa20aabe127ccaa9ec7f6
Ricartho Franck <ricartho.franck@esih.edu>
2019-09-26 14:06:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function showRepositories(){
   var repos = JSON.parse(this.responseText);
-  const repoList = `<ul>${repos.map(r => '<li>' + r.name + '</li>').join('')}</ul>`;
+  const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="'+r.name'" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`;
   document.getElementById('repositories').innerHTML = repoList;
 }
 function getRepositories(){
