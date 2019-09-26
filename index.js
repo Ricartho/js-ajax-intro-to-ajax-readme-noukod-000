830ac0cbaf0b26161aa6a52a8ced87d69e46da3f
Ricartho Franck <ricartho.franck@esih.edu>
2019-09-26 14:30:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function getCommits(el){
   const name = el.dataset.repo;
   const req = new XMLHttpRequest();
   req.addEventListener('load',showCommits);
-  req.open('GET','https://api.github.com/users/octocat/' + name + '/commits');
+  req.open('GET', 'https://api.github.com/repos/octocat/' + name + '/commits');
   req.send();
 }
 
